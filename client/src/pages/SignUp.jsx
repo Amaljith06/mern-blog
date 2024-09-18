eb6419579d0c4bbb368cf4b22d0bca44a2db036d
Amaljith Kumar <amaljithkumar006@gmail.com>
2024-09-18 14:37:42 +0530

    adding functionality to the sign up page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,49 @@
-import { Button, Label, TextInput } from "flowbite-react";
-import React from "react";
-import { Link } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [error, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  // update on changes
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  // Submit button functionality
+  const handleSubmit = async (e) => {
+    setLoading(true);
+    setErrorMessage(null);
+    e.preventDefault();
+    // Unfilled from error
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage("Please fill out all fields!");
+    }
+    try {
+      // Sending request to server
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      // response and error handling
+      const data = await res.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      // navigate user to sign in
+      if (res.ok) {
+        navigate("/sign-in");
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
@@ -20,33 +61,70 @@ export default function SignUp() {
             Google.
           </p>
         </div>
-        
+
         {/* Right */}
         {/* Sign Up Form */}
         <div className="flex-1">
-          <form className="flex flex-col gap-4">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div>
-              <Label value="Your username"/>
-              <TextInput type="text" placeholder="Username" id="username" />
+              <Label value="Your username" />
+              <TextInput
+                type="text"
+                placeholder="Username"
+                id="username"
+                onChange={handleChange}
+              />
             </div>
             <div>
-              <Label value="Your email"/>
-              <TextInput type="text" placeholder="name@company.com" id="email" />
+              <Label value="Your email" />
+              <TextInput
+                type="email"
+                placeholder="name@company.com"
+                id="email"
+                onChange={handleChange}
+              />
             </div>
             <div>
-              <Label value="Your password"/>
-              <TextInput type="text" placeholder="Password" id="password" />
+              <Label value="Your password" />
+              <TextInput
+                type="password"
+                placeholder="Password"
+                id="password"
+                onChange={handleChange}
+              />
             </div>
+
             {/* Sign Up Button */}
-            <Button gradientDuoTone='pinkToOrange' type='submit'>
-              Sign Up
+            <Button
+              gradientDuoTone="pinkToOrange"
+              type="submit"
+              disable={loading}
+            >
+              {/* Loading Effect */}
+              {loading ? (
+                <>
+                  <Spinner size="sm" />
+                  <span className="pl-3">Loading...</span>
+                </>
+              ) : (
+                "Sign Up"
+              )}
             </Button>
+
+            {/* Sign In Shortcut */}
             <div className="flex gap-2 text-sm mt-5">
               <span>Have an account?</span>
-              <Link to='/sign-in' className="text-blue-500">
+              <Link to="/sign-in" className="text-blue-500">
                 Sign In
               </Link>
             </div>
+
+            {/* Error Message Alert*/}
+            {error && (
+              <Alert className="mt-5" color="failure">
+                {error}
+              </Alert>
+            )}
           </form>
         </div>
       </div>
