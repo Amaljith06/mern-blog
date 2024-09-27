07b470a84c5689fdddee98716b5dc11df6ac6f5b
Amaljith Kumar <amaljithkumar006@gmail.com>
2024-09-27 20:20:27 +0530

    create get posts api route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,6 +1,7 @@
 import Post from "../models/post.model.js";
 import { errorHandler } from "../utils/error.js";
 
+// create a post
 export const create = async (req, res, next) => {
   // check if user is admin
   if (!req.user.isAdmin) {
@@ -27,6 +28,57 @@ export const create = async (req, res, next) => {
   try {
     const savedPost = await newPost.save(); // save to DB
     res.status(201).json(savedPost);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// get posts
+export const getPosts = async (req, res, next) => {
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.order === "asc" ? 1 : -1;
+
+    // get the posts
+    const posts = await Post.find({
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.slug && { slug: req.query.slug }),
+      ...(req.query.postId && { _id: req.query.postId }),
+      ...(req.query.searchTerm && {
+        // search for terms in title and content
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: "i" } },
+          { content: { $regex: req.query.searchTerm, $options: "i" } },
+        ],
+      }),
+    }).sort({ updatedAt: sortDirection }).skip(startIndex).limit(limit);
+
+    // total number of posts
+    const totalPosts = await Post.countDocuments();
+
+    // present date
+    const now = new Date();
+
+    // date one month ago
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+
+    // number of posts from last month
+    const lastMonthPosts = await Post.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    res.status(200).json({
+      posts,
+      totalPosts,
+      lastMonthPosts,
+    });
+
   } catch (error) {
     next(error);
   }
